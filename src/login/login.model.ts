ef04367402cddcaf904cb106e08cae3981d311fa
vishnucodezz <vishnuhshriramk@gmail.com>
2023-07-05 15:33:49 +0530

    For Test

--- src/login/login.model.ts
+++ src/login/login.model.ts
@@ -6,7 +6,7 @@ export default class LoginModel {
   constructor(@Inject('prisma') private prisma: PrismaClient) {}
 
   public async login(name:string, email:string, password:string) {
-    console.log('POST Data:', "name :", name,"email:", email, "password:",password);
+    console.log('POST Data:', "name :", name,"email :", email, "password :",password);
     return await this.prisma.user.create({
       data: {
         name,
